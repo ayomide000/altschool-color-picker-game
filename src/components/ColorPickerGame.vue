0fb09fb1b20fb656553a03b574a2401b617e6522
Akande Gbolahan <akandegbolahanayomide@gmail.com>
2023-02-07 01:21:31 +0100

    added files

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+// import { ref, reactive } from "@vue/reactivity";
+import useColorPickerGame from "../composables/useColorPickerGame.js";
+
+export default {
+  setup() {
+   const { colors, message, matchColor} = useColorPickerGame()
+   return { colors, message, matchColor}
+  },
+};
+</script>
+
+<style></style>
